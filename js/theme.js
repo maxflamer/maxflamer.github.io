fdbf32a99fe007beeb2adb9797fc883b8f56c4cf
maxflamer <33485922+maxflamer@users.noreply.github.com>
2021-01-18 20:48:03 +0530

    Redesigned the website

--- js/theme.js
+++ js/theme.js
@@ -75,30 +75,30 @@
     }
     mailChimp();
 	
-	$('select').niceSelect();
+	//$('select').niceSelect();
 	
 	/*----------------------------------------------------*/
     /*  Simple LightBox js
     /*----------------------------------------------------*/
-    $('.imageGallery1 .light').simpleLightbox();
+    // $('.imageGallery1 .light').simpleLightbox();
 	
-	$('.counter').counterUp({
-		delay: 10,
-		time: 1000
-	});
+	// $('.counter').counterUp({
+		// delay: 10,
+		// time: 1000
+	// });
 	
-	$(".skill_main").each(function() {
-        $(this).waypoint(function() {
-            var progressBar = $(".progress-bar");
-            progressBar.each(function(indx){
-                $(this).css("width", $(this).attr("aria-valuenow") + "%")
-            })
-        }, {
-            triggerOnce: true,
-            offset: 'bottom-in-view'
+	// $(".skill_main").each(function() {
+        // $(this).waypoint(function() {
+            // var progressBar = $(".progress-bar");
+            // progressBar.each(function(indx){
+                // $(this).css("width", $(this).attr("aria-valuenow") + "%")
+            // })
+        // }, {
+            // triggerOnce: true,
+            // offset: 'bottom-in-view'
 
-        });
-    });
+        // });
+    // });
 	
 	
 	/*----------------------------------------------------*/
